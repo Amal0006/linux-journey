a58591aaf9379b8d2706cd23c18f98efa41cdf17
Amal A S <amalas2875@gmail.com>
2026-06-04 05:31:45 +0000

    Week 2 day 1 - First Python Script, Calculator

---
+++ calculator.py
@@ -0,0 +1,8 @@
+
+num1=float(input("first num? "))
+num2=float(input("second num? "))
+
+print("Addition ",num1+num2)
+print("subtraction ",num1-num2)
+print("Multiplication ",num1*num2)
+print("Divisio ",num1/num2)
